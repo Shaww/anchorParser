37ed80b2e7aa193cc796c03a9c6fecb4e3754265
Shaw W Chu <shawwchu@gmail.com>
2016-04-04 10:55:07 +1200

    Reimplemented the creation of the AST using explicit returns from functions.
    * Changed AST creation from trying to manipulate ast state via implicit return
      (via puesdo stack juggling using temp variables) actions to just using explicit
      expressions as the result of function/method invocation.

--- src/anchor-parser.js
+++ src/anchor-parser.js
@@ -36,12 +36,15 @@ class AnchorParser {
         if (this.lookaheadToken.type === expectedType) {
             let node = new AST(this.lookaheadToken);
 
-            this.ast = node;
             this.lookaheadToken = this.source.nextToken(); 
+
+            return node;
         } else {
-            let inputTypeName       = tokenName(this.lookaheadToken.type),
-                expectedTypeName    = tokenName(expectedType);
+            let inputTypeName     = tokenName(this.lookaheadToken.type),
+                expectedTypeName  = tokenName(expectedType);
                 
+            this.ast = null;
+
             throw Error('Expecting ' + expectedTypeName + ' type; found ' +
                         inputTypeName + ' type.'); 
         } 
@@ -51,84 +54,92 @@ class AnchorParser {
         return (this.lookaheadToken.type === expectedType);
     }
 
+    asTree() {
+        return this.ast; 
+    }
+
     fragmentStart() {
-        let tree;
+        let ast;
 
-        this.fragments();
-        tree = this.ast;
-
+        ast = this.fragments();
         this.match(TokenEnum.EOL);
-        this.ast = tree;
+
+        this.ast = ast;
+        return ast;
     }
 
     fragments() {
-        this.fragment();
+        let singleContext;
+
+        singleContext = this.fragment();
         
         while (this.check(TokenEnum.AMPERSAND)) {
-            let tree = this.ast;
+            let ampersandNode, additionalContextSubtree;
 
-            this.match(TokenEnum.AMPERSAND);
-            this.ast.addChild(tree);
-            tree = this.ast;
+            ampersandNode               = this.match(TokenEnum.AMPERSAND);
+            additionalContextSubtree    = this.fragments();
 
-            this.fragments();
-            tree.addChild(this.ast);
-            this.ast = tree;
+            ampersandNode.addChild(singleContext);
+            ampersandNode.addChild(additionalContextSubtree);
+
+            return ampersandNode;
         }
+
+        return singleContext;
     }
 
     fragment() {
-        let tree;
+        let assignNode, leftOperand, rightOperand;
 
-        this.match(TokenEnum.WORD); 
-        tree = this.ast;
+        leftOperand     = this.match(TokenEnum.WORD); 
+        assignNode      = this.match(TokenEnum.EQUALS); 
+        rightOperand    = this.match(TokenEnum.WORD); 
 
-        this.match(TokenEnum.EQUALS); 
-        this.ast.addChild(tree);
-        tree = this.ast;
+        assignNode.addChild(leftOperand);
+        assignNode.addChild(rightOperand)
 
-        this.match(TokenEnum.WORD); 
-        tree.addChild(this.ast);
-        this.ast = tree;
+        if  (this.check(TokenEnum.COLON)) {
+            let colonNode, dependentSubtree;
 
-        while (this.check(TokenEnum.COLON)) {
-            let tree = this.ast;
+            colonNode           = this.match(TokenEnum.COLON);
+            dependentSubtree    = this.dependentParts();
 
-            this.match(TokenEnum.COLON);
-            this.ast.addChild(tree);
-            tree = this.ast;
+            colonNode.addChild(assignNode);
+            colonNode.addChild(dependentSubtree);
 
-            this.dependentParts();
-            tree.addChild(this.ast);
-            this.ast = tree;
+            // there is a dependent part, so the colon node(the separtator) 
+            // becomes the new local root, with the assignment subtree being
+            // it's left child.
+            return colonNode;
         }
+
+        // assignment node is the local root;
+        return assignNode;
     }
 
     dependentParts() {
-        let tree;
+        let keyNode, valueNode, commaNode;
 
-        this.match(TokenEnum.WORD); 
-        tree = this.ast;
+        keyNode     = this.match(TokenEnum.WORD); 
+        commaNode   = this.match(TokenEnum.COMMA); 
+        valueNode   = this.match(TokenEnum.WORD); 
 
-        this.match(TokenEnum.COMMA); 
-        this.ast.addChild(tree);
-        tree = this.ast;
-
-        this.match(TokenEnum.WORD); 
-        tree.addChild(this.ast);
-        this.ast = tree;
+        commaNode.addChild(keyNode);
+        commaNode.addChild(valueNode);
 
         while (this.check(TokenEnum.VERTBAR)) {
-            let tree = this.ast;
+            let verticalbarNode, dependentSubtree;
 
-            this.match(TokenEnum.VERTBAR);
-            this.ast.addChild(tree);
-            tree = this.ast;
+            verticalbarNode     = this.match(TokenEnum.VERTBAR);
+            dependentSubtree    = this.dependentParts();
 
-            this.dependentParts();
-            tree.addChild(tree);
-            this.ast = tree;
+            verticalbarNode.addChild(commanNode);
+            verticalbarNode.addChild(dependentSubtree);
+
+            return verticalbarNode;
         }
+
+        return commaNode;
     }
 }
 
@@ -148,7 +159,7 @@ let prefixTraversal = (root) => {
             delete node.alreadyVisited;
 
         } else if (isLeaf) {
-            buffer +=  ' ' + node.lexeme();
+            buffer += ' ' + node.lexeme();
             
         } else {
             let lastIndex       = node.children.length - 1, 
@@ -175,11 +186,12 @@ let input   = 'chat=on:uid,suzie|color,green',
     incomplete      = 'chat',
     mixed           = 'chat=on:uid,suzie&profile=2',
     complex         = 'chat=on&profile=2&debug=false',
-    lexer   = new AnchorLexxer(complex),
+    mixedComplex    = 'chat=on:uid,suzie&profile=2&debug=false:priority,high',
+    lexer   = new AnchorLexxer(mixedComplex),
     parser  = new AnchorParser(lexer);
 
 
 parser.fragmentStart();
-// console.log('ast is:', parser.ast);
+console.log('ast is:', parser.ast);
 // console.log(parser.ast);
 console.log(prefixTraversal(parser.ast));
